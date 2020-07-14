421034fe38a12d01ecda81699dd10a3d8c1ac3ea
Watsyyy <thomaswatson04@gmail.com>
2020-07-14 18:41:02 +0100

    Added initial variables for codes, sum and product. Also added some messages to print to the console.

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -4,6 +4,24 @@
 
 int main()
 {
-    
-}
+    //Declaring code variables
+    const int codeA = 4;
+    const int codeB = 3;
+    const int codeC = 2;
+    const int codeSum = codeA + codeB + codeC;
+    const int codeProduct = codeA * codeB * codeC;
 
+    //Declare game variables
+    int playerGuesses;
+
+    //Print starting message
+    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+    std::cout << "Enter the correct code to continue..." << std::endl;
+
+    //Print sum and product to the terminal
+    std::cout << "There are 3 numbers in the code" << std::endl;
+    std::cout << "The codes add up to: " << codeSum << std::endl;
+    std::cout << "The codes multiply to give: " << codeProduct << std::endl;
+
+    return 0;
+}
