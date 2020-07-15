fbdf27fbb46992e9ff8a48a471ad042300a30cde
Watsyyy <thomaswatson04@gmail.com>
2020-07-15 13:39:38 +0100

    Adding main function of game. Need to randomize the code numbers.

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -2,8 +2,17 @@
 //
 #include <iostream>
 
-int main()
+void PrintIntroduction(int LevelDifficulty)
 {
+    //Print starting message
+    std::cout << "You are a secret agent breaking into a secure server room... with a level difficulty of : " << LevelDifficulty << std::endl;
+    std::cout << "Enter the correct code to continue...\n\n";
+}
+
+bool PlayGame(int LevelDifficulty) 
+{
+    PrintIntroduction(LevelDifficulty);
+
     //Declaring code variables
     const int codeA = 4;
     const int codeB = 3;
@@ -11,17 +20,52 @@ int main()
     const int codeSum = codeA + codeB + codeC;
     const int codeProduct = codeA * codeB * codeC;
 
-    //Declare game variables
-    int playerGuesses;
-
-    //Print starting message
-    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
-
     //Print sum and product to the terminal
-    std::cout << "There are 3 numbers in the code" << std::endl;
+    std::cout << "There are 3 numbers in the code\n";
     std::cout << "The codes add up to: " << codeSum << std::endl;
     std::cout << "The codes multiply to give: " << codeProduct << std::endl;
+
+    //Declare game variables
+    int GuessA;
+    int GuessB;
+    int GuessC;
+
+    std::cout << "Please enter your three guesses: \n";
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    //Initilising guess sum and product
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    //Win/lose message
+    if (GuessSum == codeSum && GuessProduct == codeProduct)
+    {
+        std::cout << "Congragulations, you cracked the code!!!\n";
+        return true;
+    }
+    else
+    {
+        std::cout << "Sorry, the numbers did not crack the code.\n";
+        return false;
+    }
+}
+
+int main()
+{
+
+    int LevelDifficulty = 1;
+
+    while (true) 
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete)
+        {
+            LevelDifficulty++;
+        }
+    }
 
     return 0;
 }
