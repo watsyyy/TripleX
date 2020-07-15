278029a93c49b2e615d548926c8edb885c59618c
Watsyyy <thomaswatson04@gmail.com>
2020-07-15 16:03:26 +0100

    Final check-in for TripleX - Using rand() function for codes.

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,22 +1,23 @@
 // TripleX.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
+#include <ctime>
 
-void PrintIntroduction(int LevelDifficulty)
+void PrintIntroduction(int Difficulty)
 {
     //Print starting message
-    std::cout << "You are a secret agent breaking into a secure server room... with a level difficulty of : " << LevelDifficulty << std::endl;
+    std::cout << "You are a secret agent breaking into a secure server room... with a level difficulty of : " << Difficulty << std::endl;
     std::cout << "Enter the correct code to continue...\n\n";
 }
 
-bool PlayGame(int LevelDifficulty) 
+bool PlayGame(int Difficulty) 
 {
-    PrintIntroduction(LevelDifficulty);
+    PrintIntroduction(Difficulty);
 
     //Declaring code variables
-    const int codeA = 4;
-    const int codeB = 3;
-    const int codeC = 2;
+    const int codeA = rand() % Difficulty + Difficulty;
+    const int codeB = rand() % Difficulty + Difficulty;
+    const int codeC = rand() % Difficulty + Difficulty;
     const int codeSum = codeA + codeB + codeC;
     const int codeProduct = codeA * codeB * codeC;
 
@@ -37,25 +38,26 @@ bool PlayGame(int LevelDifficulty)
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
-    //Win/lose message
+    //Win-lose message
     if (GuessSum == codeSum && GuessProduct == codeProduct)
     {
-        std::cout << "Congragulations, you cracked the code!!!\n";
+        std::cout << "You Win! Increasing the level difficulty.\n";
         return true;
     }
     else
     {
-        std::cout << "Sorry, the numbers did not crack the code.\n";
+        std::cout << "Sorry, the numbers did not crack the code. Try Again!\n";
         return false;
     }
 }
 
 int main()
 {
-
+    srand(time(NULL)); //create new random sequence based on time.
+    const int MaxDifficulty = 5;
     int LevelDifficulty = 1;
 
-    while (true) 
+    while (LevelDifficulty <= MaxDifficulty) // Loop until all levels are completed
     {
         bool bLevelComplete = PlayGame(LevelDifficulty);
         std::cin.clear();
@@ -66,6 +68,8 @@ int main()
             LevelDifficulty++;
         }
     }
+
+    std::cout << "Congragulations, you cracked the code! Game Over.\n";
 
     return 0;
 }
